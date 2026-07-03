20df27600f45c6850c27d76bf8d4a735e554d963
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2026-07-03 14:39:15 +0300

    Add project files.

---
+++ SongCatalog/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SongCatalog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
